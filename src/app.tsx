93e8c9863f521c88b7b8682b9b452b7ca7433989
César Alberca <cesalberca@gmail.com>
2021-02-05 17:33:15 +0100

    Fix api call

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,17 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState, FC } from 'react'
 import { http } from './http'
 
-export function App() {
+export const App: FC = () => {
   const [message, setMessage] = useState('')
 
   async function fetchMessage() {
     const { data } = await http.get('/')
-    setMessage(data.message)
+    setMessage(data.answer)
   }
 
   useEffect(() => {
     fetchMessage()
   }, [])
+
   return <div>Message: {message}</div>
 }
